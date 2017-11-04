6aa82df1e033cb3350f368965d51f5e3f97b30de
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2017-11-04 20:47:19 +0800

    Several fixes in Client
    [FIXED] 非命令反馈的显示不正确的问题
            现在客户端能正常显示服务器在任何时候发送的反馈了XD
    [REFACT] 改了改几个函数的名字

--- arcom_client.lua
+++ arcom_client.lua
@@ -1,4 +1,5 @@
 local WAIT_FOR_FEEDBACK = 0.0001
+local FEEDBACK_FLUSH_PERIOD = 1
 
 print("ARC Communication Systems Client")
 local arcom = dofile( "arcomlib.lua" )
@@ -6,22 +7,49 @@ arcom.initClient()
 
 local feedBackQueue = {}	-- If a lock is nessensarry?
 
-function interfaceLoop()
+
+-- flush everything in the FB Queue
+-- it is called periodly or right after the Client firing a cmd
+function flushFeedback ()
+	if #feedBackQueue > 0 then
+		-- override current prompt "arcom>"
+		term.setCursorPos(1, select(2, term.getCursorPos()))
+		while #feedBackQueue > 0 do
+			print( feedBackQueue[1] )
+			table.remove( feedBackQueue, 1 )
+		end
+		-- override current prompt "arcom>"
+		term.setCursorPos(1, select(2, term.getCursorPos()))
+		io.write("arcom>")
+	end
+end
+
+
+function interfaceThread()
 	while true do
+		-- avoid several prompts running into each other
+		term.setCursorPos(1, select(2, term.getCursorPos()))
 		io.write( "arcom>" )
 		local cmdString = read()
 		if string.len(cmdString) ~= 0 then
 			arcom.fireCmd(cmdString)
 		end
 		sleep(WAIT_FOR_FEEDBACK) -- wait for the handler to pick up feedback
-		while #feedBackQueue > 0 do
-			print( feedBackQueue[1] )
-			table.remove( feedBackQueue, 1 )
-		end
+		flushFeedback()
 	end
 end
 
-function feedbackHandler()
+
+-- Flush the feedBack Queue every few secs
+function feedBackFlusherThread()
+	while true do
+		flushFeedback()
+		sleep(FEEDBACK_FLUSH_PERIOD)
+	end
+end
+
+
+function feedbackReceiverThread()
 	while true do
 		local feedBack = {}
 		feedBack = arcom.receiveFeedback()
@@ -29,4 +57,5 @@ function feedbackHandler()
 	end
 end
 
-parallel.waitForAny(interfaceLoop, feedbackHandler)
+-- wow parallel such cool wow!
+parallel.waitForAny(interfaceThread, feedbackReceiverThread, feedBackFlusherThread)
