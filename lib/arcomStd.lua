0ee21a8958298c091086c9c7072697413ea13d2a
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2017-12-29 21:30:03 +0800

    完成所有注册函数

--- lib/arcomStd.lua
+++ lib/arcomStd.lua
@@ -3,7 +3,7 @@ arcomStd.__index = arcomStd
 
 function arcomStd.new (h_net, h_pickler)
   if type(h_net) ~= "table" or type(h_pickler) ~= "table" then
-    error("ArcomStd: new: bad args. arcomnet handle and table pickler handle required.")
+    error("ArcomStd: new(): bad args. arcomnet handle and table pickler handle required.")
   end
 
   local object = {}
@@ -20,20 +20,32 @@ function arcomStd.new (h_net, h_pickler)
 end
 
 -- Registering methods
-function arcomStd:regMainLoop (ml)
-  print("Todo!")
+function arcomStd:regLoop(loopFunc)
+  if type(loopFunc) ~= "function" then
+    error("ArcomStd: regLoop(): bad args. Loop function required.")
+  end
+  self.loopFunction = loopFunc
 end
 
-function arcomStd:regInitFunction (initF)
-  print("Todo!")
+function arcomStd:regInit(initFunc)
+  if type(initFunc) ~= "function" then
+    error("ArcomStd: regInit(): bad args. Init function required.")
+  end
+  self.initFunction = initFunc
 end
 
-function arcomStd:regSafetyProtocol (sp)
-  print("Todo!")
+function arcomStd:regSafety(safeProtc)
+  if type(safeProtc) ~= "function" then
+    error("ArcomStd: regSafety(): bad args. Safety Protocol function required.")
+  end
+  self.safetyProtocol = safeProtc
 end
 
-function arcomStd:regInterrupt (ISR, name)
-  print("Todo!")
+function arcomStd:regInt (ISR, name)
+  if type(ISR) ~= "function" or type(name) ~= "string" then
+    error("ArcomStd: regInt(): bad args. ISR function and its name required.")
+  end
+  self.ISRTable[name] = ISR
 end
 
 -- ISR interface methods
