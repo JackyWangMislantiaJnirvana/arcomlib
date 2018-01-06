087747e29ecac84bcbdd8c8fdbf585f0bdfa3cc7
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2018-01-06 14:53:14 +0800

    [ADDED]pickler测试用例

---
+++ examples/picklerTester.lua
@@ -0,0 +1,10 @@
+local Pickler = dofile("/lib/pickler.lua")
+local p = Pickler.new("wow.conf")
+local t = {a="b", b="c", c="d"}
+print("p:dump()")
+p:dump(t)
+print("p:read()")
+local tb = p:get()
+for k, v in pairs(tb) do
+  print(k.."="..v)
+end
