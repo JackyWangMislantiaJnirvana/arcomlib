c9ec84e587b0fc950e5ff4dbcaabb805f0d66039
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2018-03-02 17:03:46 +0800

    添加M2M发送反馈的测试

--- Testers/M2MTester_Sender.lua
+++ Testers/M2MTester_Sender.lua
@@ -2,6 +2,7 @@ local M2M = dofile("/lib/M2M.lua")
 local m2m = M2M.new("sender", "top")
 
 m2m:sendMsg("receiver", "test", "Hello, Arcomlib!")
+m2m:sendFeedBack("TEST", "Test feedback!")
 
 -- Test wrong hostname
 m2m:sendMsg("greg", "test", "Hello, Greg!")
